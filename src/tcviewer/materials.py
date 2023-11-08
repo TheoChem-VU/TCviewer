33f6bd50610e7c7c9b69e49d3a48421ead302afd
Yuman Hordijk <yumanhordijk@gmail.com>
2023-11-08 12:09:19 +0100

    Added some new materials for atoms

--- src/tcviewer/materials.py
+++ src/tcviewer/materials.py
@@ -17,3 +17,21 @@ orbital_matte.base_reflectance = .3
 orbital_matte.base_clearcoat = 0
 orbital_matte.thickness = 1
 orbital_matte.transmission = 0
+
+atom_shiny = o3d.visualization.rendering.MaterialRecord()
+# atom_shiny.shader = 'defaultLit'
+atom_shiny.base_color = [1, 1, 1, 1]
+atom_shiny.base_roughness = .8
+atom_shiny.base_reflectance = .3
+atom_shiny.base_clearcoat = 1
+atom_shiny.thickness = 1
+atom_shiny.transmission = 0
+
+atom_matte = o3d.visualization.rendering.MaterialRecord()
+# atom_matte.shader = 'defaultLit'
+atom_matte.base_color = [1, 1, 1, 1]
+atom_matte.base_roughness = 1
+atom_matte.base_reflectance = 0
+atom_matte.base_clearcoat = 0
+atom_matte.thickness = 1
+atom_matte.transmission = 0
