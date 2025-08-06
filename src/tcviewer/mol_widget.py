33cb12d63d81b1ad07ccad010c23bcef4b081a37
Yuman Hordijk <yumanhordijk@gmail.com>
2025-08-06 12:53:53 +0200

    Added the enable_transparency option for screenshots

--- src/tcviewer/mol_widget.py
+++ src/tcviewer/mol_widget.py
@@ -50,14 +50,16 @@ class MoleculeScene:
     def __exit__(self, *args):
         self.reset_camera()
         self.post_draw()
-        pass
 
-    def screenshot(self, path: str, scale=4):
+    def screenshot(self, path: str, scale=4, enable_transparency=True):
         self.post_draw()
         img_filter = vtk.vtkWindowToImageFilter()
         img_filter.SetInput(self.parent.renWin)
         img_filter.SetScale(scale)
-        img_filter.SetInputBufferTypeToRGBA()
+        if enable_transparency:
+            img_filter.SetInputBufferTypeToRGB()
+        else:
+            img_filter.SetInputBufferTypeToRGBA()
         img_filter.ReadFrontBufferOff()
         img_filter.Update()
 
