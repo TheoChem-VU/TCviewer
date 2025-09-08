1a7fc51754f931ba313a463d36c5a422e40a3dba
Yuman Hordijk <yumanhordijk@gmail.com>
2025-09-08 16:13:25 +0200

    Added the new screenwindow

--- src/tcviewer/screen.py
+++ src/tcviewer/screen.py
@@ -8,23 +8,25 @@ except ImportError:
 
 
 class Screen:
-    def __new__(cls, headless=False):
+    def __new__(cls, headless=False, existing_app=None):
         if headless or not has_qt:
             return _HeadlessScreen()
         else:
-            return _Screen()
+            if existing_app is None:
+                return _Screen()
+            else:
+                return _ScreenWindow(existing_app)
 
 
 class _HeadlessScreen:
     def __init__(self):
         self.molview = mol_widget.MoleculeWidget(headless=True)
-        self.use_parallel_projection = False
+        self.use_parallel_projection = True
 
     def __enter__(self):
         return self
 
     def __exit__(self, *args):
-        # self.exec()
         pass
 
     def draw_molecule(self, *args, **kwargs):
@@ -44,7 +46,7 @@ class _HeadlessScreen:
 if has_qt:
     class _Screen(QtWidgets.QApplication):
         def __post_init__(self):
-            self.use_parallel_projection = False
+            self.use_parallel_projection = True
             self.window = QtWidgets.QMainWindow()
             self.window.layout = QtWidgets.QGridLayout()
             grid_widget = QtWidgets.QWidget()
@@ -100,6 +102,69 @@ if has_qt:
 
         def screenshots(self, *args, **kwargs):
             self.molview.screenshots(*args, **kwargs)
+
+    class _ScreenWindow(QtWidgets.QMainWindow):
+        def __post_init__(self):
+            self.use_parallel_projection = True
+            self.layout = QtWidgets.QGridLayout()
+            grid_widget = QtWidgets.QWidget()
+            grid_widget.setLayout(self.layout)
+            self.setCentralWidget(grid_widget)
+            self.setWindowTitle("TCViewer 2.0")
+            
+            # set up the settings screen
+            self.settings = settings.DefaultSettings()
+            self.layout.addWidget(self.settings, 0, 3, 2, 1)
+
+            # set up the molecule screen
+            self.molview = mol_widget.MoleculeWidget(self)
+            self.layout.addWidget(self.molview, 0, 0, 1, 3)
+
+            # make a scrollbar for changing the molecule
+            self.molviewslider = QtWidgets.QScrollBar()
+            self.molviewslider.setOrientation(QtCore.Qt.Horizontal)
+            self.molviewslider.setMinimum(0)
+            self.molviewslider.setMaximum(0)
+            self.molviewslider.valueChanged.connect(self.molview.set_active_mol)
+            self.layout.addWidget(self.molviewslider, 1, 1)
+
+            self.molviewslider_prevbtn = QtWidgets.QPushButton('<')
+            self.molviewslider_prevbtn.clicked.connect(self.molview.previous_mol)
+            self.molviewslider_nextbtn = QtWidgets.QPushButton('>')
+            self.molviewslider_nextbtn.clicked.connect(self.molview.next_mol)
+            self.layout.addWidget(self.molviewslider_prevbtn, 1, 0)
+            self.layout.addWidget(self.molviewslider_nextbtn, 1, 2)
+
+            self.layout.setColumnStretch(0, 0)
+            self.layout.setColumnStretch(1, 1)
+            self.layout.setColumnStretch(2, 0)
+            self.isclosed = False
+
+        def closeEvent(self, event):
+            self.isclosed = True
+            event.accept()
+
+        def __enter__(self):
+            self.__post_init__()
+            return self
+
+        def __exit__(self, *args):
+            self.window.show()
+            self.exec()
+            self.shutdown()
+
+        def draw_molecule(self, *args, **kwargs):
+            self.molview.draw_molecule(*args, **kwargs)
+
+        def add_molscene(self):
+            return self.molview.new_scene()
+
+        def screenshot(self, *args, **kwargs):
+            self.molview.screenshot(*args, **kwargs)
+
+        def screenshots(self, *args, **kwargs):
+            self.molview.screenshots(*args, **kwargs)
+
 
 
 if __name__ == '__main__':
